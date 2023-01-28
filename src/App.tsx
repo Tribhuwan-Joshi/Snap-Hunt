80d59b09f065de3ef024e32380f27d7a86e90b46
Tribhuwan <mathincosmos@gmail.com>
2023-01-28 14:23:20 +0530

    Create sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import univ from "./assets/univ.jpg"
 
-function App() {
+function Header(){
+  
+  return <div className='h-[7vh] md:h-[10vh] sticky top-0 z-50  w-full bg-gray-900 border-red-400  border-b-2 '>
+
+  </div>
+}
+function Main(){
+  return <div className='main-app'>
+  <img src={univ} alt="univ" />
+  </div>
+}
+
+function App():React.ReactElement {
   return (
-    <div className="App bg-red-400">
-     Hello World
-    </div>
-  );
+    <>
+    <Header />
+    <Main/>
+    
+    </>)
 }
 
 export default App;
---
+++ src/Hello.tsx
@@ -0,0 +1,8 @@
+type HelloProps = {title:String};
+function Hello({title} :HelloProps){
+    return <h1>{title}</h1>
+    
+
+}
+
+export default Hello;
