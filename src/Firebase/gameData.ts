8289c9907574ab10677191810894348f43b6cff1
Tribhuwan <mathincosmos@gmail.com>
2023-03-04 09:57:46 +0530

    optimize firestore operations

--- src/Firebase/gameData.ts
+++ src/Firebase/gameData.ts
@@ -1,57 +1,8 @@
-import {
-  setDoc,
-  addDoc,
-  getDoc,
-  getDocs,
-  doc,
-  collection,
-} from "firebase/firestore";
+import { addDoc, getDocs, collection } from "firebase/firestore";
 import { db } from "./firebase-config";
 
 const locationRef = collection(db, "location");
 const leaderBoardRef = collection(db, "leaderboard");
-interface characterPos {
-  name: string;
-  x: number;
-  y: number;
-}
-
-const charactersLoc: Array<characterPos> = [
-  {
-    name: "spike",
-    x: 59.7,
-    y: 77.1,
-  },
-  {
-    name: "stewie",
-    x: 93.55,
-    y: 76.63,
-  },
-  {
-    name: "tom",
-    x: 84.1,
-    y: 94.5,
-  },
-];
-
-(async () => {
-  try {
-    await Promise.all(
-      charactersLoc.map(async (c: characterPos) => {
-        const characterDocRef = doc(locationRef, c.name); // create a new document on collection reference locationRef with id c.name
-        const characterDoc = await getDoc(characterDocRef); // get its data using getDoc with reference
-        if (!characterDoc.exists()) {
-          await setDoc(characterDocRef, c);
-          return;
-        }
-        // setDoc on document reference created using doc
-      })
-    );
-  } catch (err: unknown) {
-    if (err instanceof Error) console.log(err.message);
-    else console.log("Unknown error occurred");
-  }
-})();
 
 async function checkLocation(name: string, x: number, y: number) {
   const data = await getDocs(locationRef);
@@ -67,7 +18,6 @@ async function checkLocation(name: string, x: number, y: number) {
 }
 
 async function addUser(name: string, totalTime: number) {
- 
   await addDoc(leaderBoardRef, { name, totalTime });
 }
 
